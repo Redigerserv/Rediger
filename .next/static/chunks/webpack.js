40bb412a00f3ea6802786b3ca94e1aaa129c740c
varunpunzuri <varun.punzur@fmssolutions.com>
2024-07-17 19:13:44 +0530

    font family changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a95120dde217bb8"; }
+/******/ 		__webpack_require__.h = function() { return "9aef6ea4abcea33b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import Head from 'next/head';
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/layout";
 // styles
@@ -38,6 +39,11 @@ import '@/assets/scss/_faq.scss'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+     <Head>
+     <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
